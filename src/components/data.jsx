3c5f3eda9a9948d28baf936006ed3c2ce7a28ebd
Milyaz Kamil <milyazkamil@gmail.com>
2023-11-29 10:21:43 +0300

    Created a data array containing information about phones.

---
+++ src/components/data.jsx
@@ -0,0 +1,36 @@
+import image1 from "../assets/images/phone-1.png";
+import image2 from "../assets/images/phone-2.png";
+import image3 from "../assets/images/phone-3.png";
+import image4 from "../assets/images/phone-4.png";
+
+
+export const cartItems = [
+  {
+    id: 1,
+    title: 'Samsung Galaxy S8',
+    price: 399.99,
+    img: image1,
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'Google Pixel',
+    price: 499.99,
+    img: image2,
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    img: image3,
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: 'Samsung Galaxy S7',
+    price: 599.99,
+    img: image4,
+    amount: 1,
+  },
+];
