739e1b79fd6e9ef1c11c4fdfd9a602208ef9d322
Milyaz Kamil <milyazkamil@gmail.com>
2023-11-29 11:56:53 +0300

    Created a hero component that contains products.

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import Products from "./Products";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h2 className="title">YOUR BAG</h2>
+      <Products />
+    </section>
+  )
+}
+export default Hero;
